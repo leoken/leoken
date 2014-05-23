259bd06278b47b7695696cca4270c9374418ec63
Erik James Albaugh <erik@maintainweb.co>
2014-05-23 16:50:17 -0700

    Switch to Inverse Navbar
    Switch to dark navbar

--- lib/scripts.php
+++ lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '0053158f1ffccb342108471381e61290');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '989ebfbb0be24720449c54f867114641');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
